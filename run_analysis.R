e1e2b0b9c3de816e3b0db404f1a1dc03ab8ac1c5
lunas <lukasnick@gmail.com>
2014-04-17 19:25:34 +0200

    merging train and test data, selecting mean and std columns, label activity

--- run_analysis.R
+++ run_analysis.R
@@ -5,14 +5,60 @@
 ## Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
 # Todo
-#
-# merge test/X_test.txt and train/X_train.txt into X.txt (rbind), with factor test.train
-#   these files are fixed width, each column=16 chars
-# merge test/y_test.txt and train/y_train.txt into y.txt (rbind), with factor test.train
-#   these files are fixed width, each column=16 chars
-# merge test/subject_test.txt and train/subject_train.txt into subject.txt, with factor test.train
-# merge X.txt and y.txt into Xy.txt (cbind)
-# merge Xy.txt and subject.txt into Xysub.txt. Should result in (7352+2947) x (561 + 1 + 1) table
 # 
-# Drop all columns except y (activity), subject, and the mean and std columns
+# label factor activity with descriptive levels
+# label columns?
+# ---------------
+# aggregate: mean of each variable by subject and activity
 
+
+run_analysis <- function(directory = "uci-har-dataset") {
+  setwd(directory)
+  
+  # read features names
+  features <- read.table("features.txt", col.names=c("f.id", "feature"))
+  
+  ####### TEST data ##########################################
+  # read fixed width file X_test
+  widths <- rep(16, 561)
+  X.test <- read.fwf("test/X_test.txt", widths, col.names=features$feature)
+  # add factor to designate these cases as test cases:
+  X.test$test.train <- as.factor(rep("test", dim(X.test)[1]))
+  
+  # read subject data and add it to X.test
+  subject.test <- read.table("test/subject_test.txt", col.names="subject")
+  X.test$subject <- subject.test$subject
+
+  # read y data and add it to X.test as "activity"
+  y.test <- read.table("test/y_test.txt", col.names="activity")
+  X.test$activity <- y.test$activity
+  
+  ####### TRAIN data ##########################################
+  
+  X.train <- read.fwf("train/X_train.txt", widths, col.names=features$feature)
+  X.train$test.train <- as.factor(rep("train", dim(X.train)[1]))
+  
+  # read subject data and add it to X.test
+  subject.train <- read.table("train/subject_train.txt", col.names="subject")
+  X.train$subject <- subject.train$subject
+  
+  # read y data and add it to X.train
+  y.train <- read.table("train/y_train.txt", col.names="activity")
+  X.train$activity <- y.train$activity
+  
+  
+  # combine X.test and X.train into one data frame
+  X <- rbind(X.test, X.train)
+  
+  # only keep mean and std columns 
+  mean.cols <- grep("mean\\.", colnames(X), value=TRUE)
+  std.cols  <- grep("std\\.",  colnames(X), value=TRUE)
+  keep = c("subject", "test.train", "activity", mean.cols, std.cols)
+  X <- X[keep]
+  
+  # label activity
+  labels <- c("walking", "walking.upstairs", "walking.downstairs", "sitting", "standing", "laying")
+  activity <- factor( X$activity, levels=c(1:6), labels=labels, ordered=TRUE)
+  X$activity <- activity
+  X
+}
